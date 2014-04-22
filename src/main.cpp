5c7924a2346e7d6abc3d40ef02edb4a3a5b1e5a5
r3nic1e <r3nic1e@me.com>
2014-04-23 00:47:54 +0400

    Version with accessing files and printing its contents

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,50 @@
 #include <ncurses.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+fstream *openFile(char *filename)
+{
+	fstream *file = new fstream;
+	file->open(filename);
+	return file;
+}
+
+int printFile(fstream *file)
+{
+	char buffer[256];
+	while (!file->eof())
+	{
+		file->getline(buffer, 256);
+		addstr(buffer);
+		int x, y;
+		getyx(stdscr, y, x);
+		move(0, y + 1);
+		insdelln(1);
+	}
+	return 0;
+}
+
+int closeFile(fstream *file)
+{
+	file->close();
+	delete file;
+	return 0;
+}
 
 int main(int argc, char **argv)
 {
+	if (argc <= 1) return 0;
+	char *filename = argv[1];
+	fstream *file = openFile(filename);
 	initscr();
-	move(10, 25);
-	printw("Hi world!\n");
+	move(0, 0);
+	printFile(file);
 	refresh();
 	getch();
+	closeFile(file);
 	endwin();
 	return 0;
 }
